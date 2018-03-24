adc6a10a185d2d3dff07e3966b6a47d76cb7f082
Felipe <felipe@debian.Felipe>
2018-03-24 12:41:15 -0300

    again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import static spark.Spark.*;
 
 import model.Model;
 import view.View;
@@ -14,6 +15,16 @@ public class Main {
 		View view = new View(model);
 		model.registerObserver(view); // connection Model -> View
 		view.receiveUsersMessages();
+		
+		 ProcessBuilder process = new ProcessBuilder();
+	        int myPort;
+	        if (process.environment().get("PORT") != null) {
+	            myPort = Integer.parseInt(process.environment().get("PORT"));
+	        } else {
+	            myPort = 8080;
+	        }
+	        port(myPort);
+
 
 	}
 
